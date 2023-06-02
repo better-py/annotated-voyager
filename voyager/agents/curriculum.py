dac7d8730b51ab4999a838d1a14960f4aebd78fd
Henry.Huang <hhglory@outlook.com>
2023-06-02 10:14:35 +0800

    annotated(prompts) - learn how to write prompts

--- voyager/agents/curriculum.py
+++ voyager/agents/curriculum.py
@@ -9,7 +9,7 @@ from voyager.utils.json_utils import fix_and_parse_json
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.schema import HumanMessage, SystemMessage
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import Chroma  # todo x: 向量数据库, 可以顺便查看 langchain 支持了哪些向量数据库
 
 
 class CurriculumAgent:
--- voyager/prompts/__init__.py
+++ voyager/prompts/__init__.py
@@ -3,8 +3,10 @@ import voyager.utils as U
 
 
 def load_prompt(prompt):
-    #
-    # todo x: 解析当前路径下的 prompt 文件夹， 并返回对应的 prompt 内容
-    #
-    package_path = pkg_resources.resource_filename("voyager", "")
-    return U.load_text(f"{package_path}/prompts/{prompt}.txt")
+    """
+    todo x: 解析当前路径下的 prompt 文件夹， 并返回指定 prompt 文件(txt)内容
+        1. prompts/ 文件夹下， 已对 prompts 文件翻译（后缀为 .zh.yml), 值得学习 prompts 编写技巧（质量非常高）
+        2. agents/ 源码，有对 prompts 文件的使用， 理解整个使用链路
+    """
+    package_path = pkg_resources.resource_filename("voyager", "")  # todo x: 加载当前包的路径
+    return U.load_text(f"{package_path}/prompts/{prompt}.txt")  # todo x: 加载+解析指定 prompt 文件（txt) 内容
